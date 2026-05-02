a211852d2edc0deb5a280f6ca7747db08db63322
PassionOS <passiontagane@gmail.com>
2026-05-02 09:50:17 +0200

    Remove aliases from staff.sql

--- src/sql/queries/staff.sql
+++ src/sql/queries/staff.sql
@@ -3,13 +3,21 @@ SELECT * FROM staff_member
 WHERE staff_num = :staff_num;
 
 -- :name get_staff_details :one
-SELECT s.staff_num, s.staff_fname, s.staff_lname, s.role_id,
-       COALESCE(STRING_AGG(c.campus_name, ', '), 'All Campuses') as campuses
-FROM staff_member s
-LEFT JOIN campus_staff_assignment csa ON s.staff_num = csa.staff_num
-LEFT JOIN campus c ON csa.campus_id = c.campus_id
-WHERE s.staff_num = :staff_num
-GROUP BY s.staff_num, s.staff_fname, s.staff_lname, s.role_id;
+SELECT staff_member.staff_num,
+       staff_member.staff_fname,
+       staff_member.staff_lname,
+       staff_member.role_id,
+       COALESCE(STRING_AGG(campus.campus_name, ', '), 'All Campuses') AS campuses
+FROM staff_member
+LEFT JOIN campus_staff_assignment
+       ON staff_member.staff_num = campus_staff_assignment.staff_num
+LEFT JOIN campus
+       ON campus_staff_assignment.campus_id = campus.campus_id
+WHERE staff_member.staff_num = :staff_num
+GROUP BY staff_member.staff_num,
+         staff_member.staff_fname,
+         staff_member.staff_lname,
+         staff_member.role_id;
 
 -- :name update_staff_password :affected
 UPDATE staff_member
